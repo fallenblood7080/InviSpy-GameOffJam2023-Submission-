0bc0e0a67d5a994eba5afb40d367f7c7f6203c1e
fallenblood7080 <67326508+fallenblood7080@users.noreply.github.com>
2023-11-03 23:13:17 +0530

    added cam shake script

---
+++ Assets/_Script/Game/camShake.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camShake : MonoBehaviour
+{
+    public static camShake instance;
+
+    private CinemachineVirtualCamera virtualCamera;
+    private float shakeTime;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shakeTime > 0)
+        {
+            shakeTime -= Time.deltaTime;
+            if (shakeTime <= 0)
+            {
+                CinemachineBasicMultiChannelPerlin noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                noise.m_AmplitudeGain = 0;
+            }
+        }
+    }
+    public void Shake(float intensity ,float timer)
+    {
+        CinemachineBasicMultiChannelPerlin noise = virtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        noise.m_AmplitudeGain = intensity;
+        shakeTime = timer;
+    }
+}
